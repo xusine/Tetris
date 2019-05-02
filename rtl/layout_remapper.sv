a1b84881f8bab1becca6186479eae6c4f5264586
Shanqing Lin <sqlin16@fudan.edu.cn>
2019-05-02 22:38:57 +0800

    Update layout remapper

--- rtl/layout_remapper.sv
+++ rtl/layout_remapper.sv
@@ -4,6 +4,11 @@ module layout_remapper #(
   ,parameter integer physical_width_p = 800
   ,parameter integer physical_height_p = 600
   ,parameter integer bit_depth_p = 1
+
+  ,parameter integer str_score_start_x = 20
+  ,parameter integer str_score_start_y = 20
+  ,parameter integer str_next_start_x = 20
+  ,parameter integer str_next_start_y = 40
 )(
 
   input clk_i //=36MHz
@@ -11,9 +16,6 @@ module layout_remapper #(
 
   ,output [$clog2(logic_width_p)-1:0] x_o
   ,output [$clog2(logic_height_p)-1:0] y_o
-
-  ,input str_score_v_i
-  ,input str_next_v_i
 
   ,input [bit_depth_p-1:0] r_i
   ,input [bit_depth_p-1:0] g_i
@@ -30,51 +32,43 @@ module layout_remapper #(
   assign x_o = vga_v_i ? vga_x_i >> 3 : '0;
   assign y_o = vga_v_i ? (vga_y_i >= 12 && vga_y_i < 588 ? vga_y_i >> 3 : '0) : '0;
 
-  reg [$clog2(physical_width_p)-1:0] str_score_x_r;
-  reg [$clog2(physical_height_p)-1:0] str_score_y_r;
-
-  always_ff @(posedge clk_i) begin
-    if(reset_i) begin
-      str_score_x_r <= '0;
-      str_score_y_r <= '0;
-    end
-    else if(str_score_v_i) begin
-      str_score_x_r <= vga_x_i;
-      str_score_y_r <= vga_y_i;
-    end
-  end
-
-  logic [7:0] str_score_addr_x = vga_x_i - str_score_x_r;
-  logic [5:0] str_score_addr_y = vga_x_i - str_score_y_r;
-
+  wire [7:0] str_score_addr_x = vga_x_i - str_score_start_x * 8;
+  wire [5:0] str_score_addr_y = vga_x_i - str_score_start_y * 8;
   logic [31:0] str_score_rom_out;
 
-  reg [$clog2(physical_width_p)-1:0] str_next_x_r;
-  reg [$clog2(physical_height_p)-1:0] str_next_y_r;
+  score_mem score_mem1 (
+	.address({str_score_addr_x[7:5],str_score_addr_y})
+	,.clock(clk_i)
+	,.q(str_score_rom_out)
+  );
+  
 
-  always_ff @(posedge clk_i) begin
-    if(reset_i) begin
-      str_next_x_r <= '0;
-      str_next_y_r <= '0;
-    end
-    else if(str_next_v_i) begin
-      str_next_x_r <= vga_x_i;
-      str_next_y_r <= vga_y_i;
-    end
-  end
-
-  logic [6:0] str_next_addr_x = vga_x_i - str_next_x_r;
-  logic [5:0] str_next_addr_y = vga_x_i - str_next_y_r;
-
+  wire [6:0] str_next_addr_x = vga_x_i - str_next_start_x * 8;
+  wire [5:0] str_next_addr_y = vga_x_i - str_next_start_y * 8;
   logic [31:0] str_next_rom_out;
+  next_mem next_mem1(
+	.address({str_next_addr_x[6:5],str_next_addr_y})
+	,.clock(clk_i)
+	,.q(str_next_rom_out)
+  );
+
 
   always_comb begin
-    if(vga_x_i >= str_score_x_r && vga_x_i < str_score_x_r + 256 && vga_y_i >= str_score_y_r & vga_y_i < str_score_y_r + 64)
-      vga_r_o = str_score_rom_out[str_score_addr_x[4:0]];
-    else if(vga_x_i >= str_next_x_r && vga_x_i < str_next_x_r + 128 && vga_y_i >= str_next_y_r & vga_y_i < str_next_y_r + 64)
-      vga_r_o = str_next_rom_out[str_next_addr_x[4:0]];
-    else 
-      vga_r_o = r_i;
+    if(x_o >= str_score_start_x && x_o < str_score_start_x + 32 && y_o >= str_score_start_y & y_o < str_score_start_y + 8) begin
+      vga_r_o = r_i & str_score_rom_out[str_score_addr_x[4:0]];
+      vga_g_o = g_i & str_score_rom_out[str_score_addr_x[4:0]];
+      vga_b_o = b_i & str_score_rom_out[str_score_addr_x[4:0]];
+	 end
+    else if(x_o >= str_score_start_x && vga_x_i < str_score_start_x + 16 && y_o >= str_next_start_y & y_o < str_next_start_y + 8) begin
+      vga_r_o = r_i & str_next_rom_out[str_next_addr_x[4:0]];
+      vga_g_o = g_i & str_next_rom_out[str_next_addr_x[4:0]];
+      vga_b_o = b_i & str_next_rom_out[str_next_addr_x[4:0]];
+	 end
+    else begin
+      vga_r_o = 1'b1;
+      vga_g_o = 1'b1;
+      vga_b_o = 1'b1;
+	 end
   end
 
 
