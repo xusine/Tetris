5d53e5b0a1099a456467af143f4870bc1019c504
KAISHOWERING <KAISHOWERING@users.noreply.github.com>
2019-05-02 14:39:22 +0800

    next update

--- rtl/layout_divider.sv
+++ rtl/layout_divider.sv
@@ -1,19 +1,38 @@
 module layout_divider #(
-    parameters width_p=100,
-    parameters height_p=72
+    parameter width_p=100,
+    parameter height_p=72
     
 )( 
     input [$clog2(width_p)-1:0] x_i ,// x input
     input [$clog2(height_p)-1:0] y_i ,// y input
-    output logic socre_v,//1 stands for 
+    output logic score_v,
+    output logic next_v,
+    output r_o,
+    output g_o,
+    output b_o
 );
+
 always_comb begin     
-    if (x_i<20&&x_i>80&&y_i<20&&y_i>80) begin
-        socre_v ='0;
+    if (x_i == 20&&y_i==20) begin
+        score_v ='1;
+        next_v = '0;
+        r_o = 1;
+        g_o = 0;
+        b_o = 0;
+    end
+    else if(x_i == 20&&y_i == 40) begin
+        score_v ='0;
+        next_v = '1;
+        r_o = 0;
+        g_o = 0;
+        b_o = 1;
     end
     else begin
-        socre_v ='1;
-    end
+        score_v ='0;
+        next_v = '0;
+        r_o = 0;
+        g_o = 0;
+        b_o = 0;                
 end
 
 endmodule
